31100ecfeb76faaba4f3fc9da63d91989d9e2964
Jake Paine <jake.paine@me.com>
2011-03-21 11:18:44 +0000

    Updated project for Visual Studio 2010 and removed WebKitBrowserTest

--- WebKitBrowser/Properties/AssemblyInfo.cs
+++ WebKitBrowser/Properties/AssemblyInfo.cs
@@ -32,7 +32,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("WebKitBrowser")]
-[assembly: AssemblyDescription(".NET Wrapper for WebKit")]
+[assembly: AssemblyDescription(".NET Wrapper for WebKit engine")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("WebKit .NET")]
--- WebKitCore/Properties/AssemblyInfo.cs
+++ WebKitCore/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("WebKitCore")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Core components for WebKit .NET")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("WebKitCore")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2010")]
+[assembly: AssemblyCopyright("Copyright © Peter Nelson 2010")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
