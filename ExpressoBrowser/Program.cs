78f2ac6b4d0a05594f7dc530831db1ce14b4c226
Jake Paine <jake.paine@me.com>
2011-03-21 13:40:17 +0000

    Renamed Form1

--- ExpressoBrowser/Program.cs
+++ ExpressoBrowser/Program.cs
@@ -15,7 +15,7 @@ namespace ExpressoBrowser
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainWindow());
         }
     }
 }
